7c60bbfcc27824a2b72fb21768886e8a776a60f9
trubetskoy <and.trubetskoy@gmail.com>
2017-11-28 22:18:50 +0200

    home page init

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,9 +8,14 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $categorys = $em->getRepository('AppBundle:Category')->findAll();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'categorys' => $categorys,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
+
     }
 }
