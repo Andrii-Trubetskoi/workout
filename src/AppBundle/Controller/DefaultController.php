f24b565ef278b432aa7d17f3e92a179afc07d1dd
trubetskoy <and.trubetskoy@gmail.com>
2017-11-29 15:36:01 +0200

    search by categorys

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,17 +3,19 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $categorys = $em->getRepository('AppBundle:Category')->findAll();
+        $categorysFilter = $em->getRepository('AppBundle:Category')->findCategoryByName($request);
 
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'categorys' => $categorys,
+            'categorysFilter' => $categorysFilter,
             'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
 
